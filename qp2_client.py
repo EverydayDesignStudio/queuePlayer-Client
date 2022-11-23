43667260cdde9a4f3b42852d99126697dac2fef8
Ayush-0801 <ayush19301@iiitd.ac.in>
2022-11-23 12:28:26 +0530

    Client updates

--- qp2_client.py
+++ qp2_client.py
@@ -2,6 +2,9 @@ import threading
 from threading import Timer
 from time import time
 import requests
+import websocket #import websockt library -> pip install websocket-client
+import ssl # import ssl library (native)
+import json # import json library (native)
 
 #variable to determine the user
 userID=2
@@ -11,7 +14,7 @@ bpmAdded=36
 
 #both hosted servers for queue player funcitonality
 base_url1="https://qpm-server.herokuapp.com/"
-base_url2="https://qpt-server.herokuapp.com/"
+base_url2="https://qp2-spotify-server.herokuapp.com/"
 
 playerID=""
 playing=False
@@ -24,6 +27,8 @@ msPrev=0
 seekedPlayer=0
 
 timeouter=0
+
+
 
 #function to check the active users for each queue player client
 def makeUserActive():
@@ -212,8 +217,39 @@ def infiniteloop2():
             TapBPM()
         # time.sleep(1)
 
+def infiniteloop3():
+    while True:
+        websocket.enableTrace(True) # print the connection details (for debugging purposes)
+        ws = websocket.WebSocketApp("wss://qpm-server.herokuapp.com/", # websocket URL to connect to
+                                on_message = on_message, # what should happen when we receive a new message
+                                on_error = on_error, # what should happen when we get an error
+                                on_close = on_close) # what should happen when the connection is closed
+        ws.on_open = on_open # call on_open function when the ws connection is opened
+        ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE}) # run code forever and disable the requirement of SSL certificates
+
+def on_message(ws, message): # function which is called whenever a new message comes in
+    json_data = json.loads(message) # incoming message is transformed into a JSON object
+    print(message) # printing the data (for testing purposes)
+    # print(json_data["blockHash"]) # printing a specific part of the JSON object (for testing purposes)
+    print("") # printing new line for better legibility
+
+def on_error(ws, error): # function call when there is an error
+    print(error)
+
+def on_close(ws): # function call when the connection is closed (this should not happend currently as we are staying connected)
+    print("### closed ###")
+
+def on_open(ws): # function call when a new connection is established
+    print("### open ###")
+
+    
 thread1 = threading.Thread(target=infiniteloop1)
 thread1.start()
 
 thread2 = threading.Thread(target=infiniteloop2)
 thread2.start()
+
+thread3 = threading.Thread(target=infiniteloop3)
+thread3.start()
+
+
