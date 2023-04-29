767207edb3e1ca3c8c95ef8e098dc80eb18bda14
Ayush-0801 <ayush19301@iiitd.ac.in>
2023-04-29 22:29:31 +0530

    Update qp1_client.py

--- qp1_client.py
+++ qp1_client.py
@@ -76,9 +76,6 @@ def pushBPMToQueue():
     # print("Updated Queue : ",songToBeQueued.json())
     
     print("Updated Queue : \n")
-    # if(songToBeQueued.json()["queue"]=="Already added song"):
-    #     add-=1
-    
     for ele in songToBeQueued.json()['queue']:
         print(ele)
 
