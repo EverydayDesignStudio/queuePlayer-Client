622084f6ab9420ab5a32c84178a4f3f968f07f4f
Min Y <yoomy1203@hotmail.com>
2024-04-08 13:39:55 -0700

    Update qpLauncher.sh

--- qpLauncher.sh
+++ qpLauncher.sh
@@ -3,9 +3,9 @@
 
 ### Follow instructions on https://www.instructables.com/Raspberry-Pi-Launch-Python-script-on-startup/ to add this script to Cron
 
-### @reboot sh /home/pi/Desktop/qpLauncher.sh >/home/pi/logs/cronlog 2>&1
+### @reboot sh /home/pi/Desktop/qpLauncher.sh > /home/pi/logs/cronlog 2>&1
 
 cd /
 cd /home/pi/Desktop/QPClient_Final
 sudo python qp_client_final.py
-cd/
+cd /
